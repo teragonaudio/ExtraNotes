e76d348388ecfce9aede96fa96850b9fa4b1448e
Nik Reiman <teragonaudio@mac.com>
2010-03-20 16:42:18 +0100

    Removed all junk code from view class except for necessary carbon handlers for getting and setting text

--- source/AUNotesView.cpp
+++ source/AUNotesView.cpp
@@ -16,14 +16,9 @@
 COMPONENT_ENTRY(AUNotesView)
 
 AUNotesView::AUNotesView(AudioUnitCarbonView auv) : AUCarbonViewBase(auv) {
-  m_orig_text = NULL;
-  m_orig_len = 0;
 }
 
 AUNotesView::~AUNotesView() {
-  if(m_orig_text) {
-    delete [] m_orig_text;
-  }
 }
 
 OSStatus AUNotesView::CreateUI(Float32 xoffset, Float32 yoffset) {
@@ -49,11 +44,7 @@ OSStatus AUNotesView::CreateUI(Float32 xoffset, Float32 yoffset) {
 }
 
 bool AUNotesView::exportData() {
-  FILE *fp = NULL;
-  if(fp == NULL) {
-    return false;
-  }
-  
+  /*
   Size s_in, s_out;
   if(GetControlDataSize(m_text, kControlEditTextPart, kControlEditTextTextTag, &s_in) != noErr) {
     fprintf(stderr, "Couldn't get size elements from control\n");
@@ -66,238 +57,39 @@ bool AUNotesView::exportData() {
   else {
     body[(int)s_in] = '\0';
   }
-    
+
   delete [] body;
+   */
   return true;
 }
 
-bool AUNotesView::importData() {  
-  FILE *fp = NULL;
-  if(fp == NULL) {
-    return false;
-  }
-  
-  // Find out how many bytes are in the header and body
-  unsigned int h_len;
-  {
-    unsigned int bytes = 2;
-    char header_len[bytes + 1];
-    if(fread(&header_len, sizeof(char), bytes, fp) < bytes) {
-#ifdef DEBUG
-      fprintf(stderr, "Read error getting header length\n");
-#endif
-      fclose(fp);
-      return false;
-    }
-    header_len[bytes] = '\0';
-    h_len = atoi(header_len);
-  }
-  {
-    unsigned int bytes = 4;
-    char body_len[bytes + 1];
-    if(fread(&body_len, sizeof(char), bytes, fp) < bytes) {
-#ifdef DEBUG
-      fprintf(stderr, "Read error getting body length\n");
-#endif
-      fclose(fp);
-      return false;
-    }
-    body_len[bytes] = '\0';
-    m_orig_len = atoi(body_len);
-  }
-  
-#ifdef DEBUG
-  fprintf(stderr, "%d bytes in header, %d bytes in body\n", h_len, m_orig_len);
-#endif
-  
-  // Now read in the header and body
-  char header[h_len + 1];
-  if(fread(&header, sizeof(char), h_len, fp) < h_len) {
-#ifdef DEBUG
-    fprintf(stderr, "Read error getting header\n");
-#endif
-    fclose(fp);
-    return false;
-  }    
-  header[h_len] = '\0';
-  
-  if(m_orig_text) {
-    delete [] m_orig_text;
-  }
-  m_orig_text = new char[m_orig_len + 1];
-  if(fread(m_orig_text, sizeof(char), m_orig_len, fp) < m_orig_len) {
-#ifdef DEBUG
-    fprintf(stderr, "Read error getting body\n");
-#endif
-    fclose(fp);
-    return false;
-  }    
-  m_orig_text[m_orig_len] = '\0';
+bool AUNotesView::importData() {
+  /*
   OSStatus err = SetControlData(m_text, kControlEditTextPart, kControlEditTextTextTag,
                                 m_orig_len, m_orig_text);
   if(err != noErr) {
-#ifdef DEBUG
     fprintf(stderr, "Could not set text from file\n");
-#endif
   }
   DrawOneControl(m_text);
-  
-  fclose(fp);  
-  return true;
-}
-
-bool AUNotesView::readData() {
-  char *dname = new char[MAX_FILENAME];
-  /*
-  if(!getProductDir(DEF_PRODUCT_NAME, dname)) {
-#ifdef DEBUG
-    fprintf(stderr, "Could not get product directory\n");
-#endif
-    delete [] dname;
-    return false;
-  }
   */
-  float year, daycount, index;
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_YEAR, kAudioUnitScope_Global, 0, &year);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_DAYCOUNT, kAudioUnitScope_Global, 0, &daycount);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_INDEX, kAudioUnitScope_Global, 0, &index);
-  
-  char fname[MAX_FILENAME];
-  snprintf(fname, MAX_FILENAME, "%s%4d.%03d.%03d", dname, (int)year, (int)daycount, (int)index);
-  FILE *fp = fopen(fname, "r");
-  if(fp == NULL) {
-#ifdef DEBUG
-    fprintf(stderr, "No entry for '%s'\n", fname);
-#endif
-    delete [] dname;
-    return false;
-  }
-  
-  // Find out how many bytes are in the header and body
-  unsigned int h_len;
-  {
-    unsigned int bytes = 2;
-    char header_len[bytes + 1];
-    if(fread(&header_len, sizeof(char), bytes, fp) < bytes) {
-#ifdef DEBUG
-      fprintf(stderr, "Read error getting header length\n");
-#endif
-      delete [] dname;
-      fclose(fp);
-      return false;
-    }
-    header_len[bytes] = '\0';
-    h_len = atoi(header_len);
-  }
-  {
-    unsigned int bytes = 4;
-    char body_len[bytes + 1];
-    if(fread(&body_len, sizeof(char), bytes, fp) < bytes) {
-#ifdef DEBUG
-      fprintf(stderr, "Read error getting body length\n");
-#endif
-      delete [] dname;
-      fclose(fp);
-      return false;
-    }
-    body_len[bytes] = '\0';
-    m_orig_len = atoi(body_len);
-  }
-  
-#ifdef DEBUG
-  fprintf(stderr, "%d bytes in header, %d bytes in body\n", h_len, m_orig_len);
-#endif
-  
-  // Now read in the header and body
-  char header[h_len + 1];
-  if(fread(&header, sizeof(char), h_len, fp) < h_len) {
-#ifdef DEBUG
-    fprintf(stderr, "Read error getting header\n");
-#endif
-    delete [] dname;
-    fclose(fp);
-    return false;
-  }    
-  header[h_len] = '\0';
-  
-  if(m_orig_text) {
-    delete [] m_orig_text;
-  }
-  m_orig_text = new char[m_orig_len + 1];
-  if(fread(m_orig_text, sizeof(char), m_orig_len, fp) < m_orig_len) {
-#ifdef DEBUG
-    fprintf(stderr, "Read error getting body\n");
-#endif
-    delete [] dname;
-    fclose(fp);
-    return false;
-  }    
-  m_orig_text[m_orig_len] = '\0';
-  
-  delete [] dname;
-  fclose(fp);
   return true;
 }
 
 bool AUNotesView::saveData() {
-  char *dname = new char[MAX_FILENAME];
   /*
-  if(!getProductDir(DEF_PRODUCT_NAME, dname)) {
-#ifdef DEBUG
-    fprintf(stderr, "Could not get product directory\n");
-#endif
-    delete [] dname;
-    return false;
-  }
-  */
-  float year, month, day, daycount, index;
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_YEAR, kAudioUnitScope_Global, 0, &year);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_DAY, kAudioUnitScope_Global, 0, &day);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_DAYCOUNT, kAudioUnitScope_Global, 0, &daycount);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_MONTH, kAudioUnitScope_Global, 0, &month);
-  AudioUnitGetParameter(GetEditAudioUnit(), PRM_NOTE_INDEX, kAudioUnitScope_Global, 0, &index);
-  
-  char fname[MAX_FILENAME];
-  snprintf(fname, MAX_FILENAME, "%s%4d.%03d.%03d", dname, (int)year, (int)daycount, (int)index);
-  FILE *fp = fopen(fname, "w");
-  if(fp == NULL) {
-#ifdef DEBUG
-    fprintf(stderr, "Could not open '%s' for writing\n", fname);
-#endif
-    delete [] dname;
-    return false;
-  }
-  
   Size s_in, s_out;
   if(GetControlDataSize(m_text, kControlEditTextPart, kControlEditTextTextTag, &s_in) != noErr) {
-#ifdef DEBUG
     fprintf(stderr, "Couldn't get size elements from control\n");
-#endif
   }
   char *body = new char[(int)s_in + 1];
   if(GetControlData(m_text, kControlEditTextPart, kControlEditTextTextTag, s_in, body, &s_out) != noErr) {
-#ifdef DEBUG
     fprintf(stderr, "No data in control\n");
-#endif
     delete [] dname;
     return false;
   }
   else {
     body[(int)s_in] = '\0';
   }
-  
-  // Format: YYYYMMDD:USERNAME
-  char header[48];
-  snprintf(header, 48, "%04d%02d%02d:%s", (int)year, (int)month, (int)day, getenv("USER"));
-  
-  // Format: AABBBB:HEADER:ENTRY\n
-  // Where AA is the number of characters in the header, and BBBB is the number
-  // of characters in the entry body
-  fprintf(fp, "%02d%04d%s%s\n", (int)strlen(header), (int)s_in, header, body);
-  fflush(fp);
-  fclose(fp);
-  
-  delete [] body;
-  delete [] dname;
+  */
   return true;
 }
--- source/AUNotesView.h
+++ source/AUNotesView.h
@@ -25,26 +25,15 @@ class AUNotesView : public AUCarbonViewBase {
 public:
 	AUNotesView(AudioUnitCarbonView auv);
 	~AUNotesView();
-  
-  pascal ControlKeyFilterResult	StdKeyFilterCallback(ControlRef theControl, 
-                                                     SInt16 *keyCode, SInt16 *charCode, 
-                                                     EventModifiers *modifiers);    
+    
 	virtual OSStatus CreateUI(Float32 inXOffset, Float32 inYOffset);
 
-protected:
   bool exportData();
   bool importData();
   bool readData();
   bool saveData();
+
 private:
-  ControlRef m_save_button;
-  ControlRef m_cancel_button;
-  ControlRef m_import_button;
-  ControlRef m_export_button;
-  ControlRef m_text;
-  
-  char *m_orig_text;
-  unsigned int m_orig_len;
 };
 
 #endif

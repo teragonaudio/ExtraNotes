5aee2df63bd920c85ef96064616c8941c445754f
Nik Reiman <teragonaudio@mac.com>
2010-03-20 17:51:32 +0100

    Removed a bunch of unused definitions

--- source/defaults.h
+++ source/defaults.h
@@ -10,35 +10,6 @@
 #ifndef __defaults_H
 #define __defaults_H
 
-// GUI dimensional measurements
-#define DEF_BUTTON_HEIGHT 20
-#define DEF_BUTTON_WIDTH 70
-#define DEF_TEXT_HEIGHT 300
-#define DEF_TEXT_WIDTH 450
-#define DEF_OFFSET 10
-#define DEF_PRODUCT_NAME "AUNotes"
-
-// GUI Control references
-#define CTL_TEXT 1000
-#define CTL_SAVE 1001
-#define CTL_CANCEL 1002
-#define CTL_IMPORT 1003
-#define CTL_EXPORT 1004
-
-// Parameter stuff
-enum
-{
-  PRM_NOTE_YEAR = 0,
-  PRM_NOTE_DAY,
-  PRM_NOTE_DAYCOUNT,
-  PRM_NOTE_MONTH,
-  PRM_NOTE_INDEX,
-  NUM_PARAMS
-};
-
-// Other constants
-#define MAX_FILENAME 512
-
 #ifdef DEBUG
 #define kAUNotesVersion 0xFFFFFFFF
 #else

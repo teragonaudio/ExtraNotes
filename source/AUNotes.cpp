7a22059e08ecb36ca558e721e80184cb420fb410
Nik Reiman <teragonaudio@mac.com>
2010-03-20 16:25:26 +0100

    Cleaned up a lot of junk code in main class

--- source/AUNotes.cpp
+++ source/AUNotes.cpp
@@ -17,130 +17,38 @@ COMPONENT_ENTRY(AUNotes)
 
 AUNotes::AUNotes(AudioUnit component)	: AUEffectBase(component) {
 	CreateElements();
-	Globals()->UseIndexedParameters(NUM_PARAMS);
+	Globals()->UseIndexedParameters(0);
 }
 
 AUNotes::~AUNotes() {
 }
 
-ComponentResult AUNotes::GetParameterValueStrings(AudioUnitScope inScope,
-                                                  AudioUnitParameterID inParameterID,
-                                                  CFArrayRef *outStrings) {
-  return AUEffectBase::GetParameterValueStrings(inScope, inParameterID, outStrings);
-}
-
-ComponentResult AUNotes::GetParameterInfo(AudioUnitScope inScope,
-                                          AudioUnitParameterID inParameterID,
-                                          AudioUnitParameterInfo &outParameterInfo) {
-	ComponentResult result = noErr;
-  
-	outParameterInfo.flags = kAudioUnitParameterFlag_IsWritable |
-    kAudioUnitParameterFlag_IsReadable;
-  
-  if(inScope == kAudioUnitScope_Global) {
-    switch(inParameterID) {
-        /*
-      case PRM_NOTE_YEAR:
-        AUBase::FillInParameterName(outParameterInfo, PRM_NOTE_YEAR_NAME, false);
-        outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-        outParameterInfo.minValue = 2005;
-        outParameterInfo.maxValue = 2010;
-        outParameterInfo.defaultValue = m_year;
-        break;
-      case PRM_NOTE_DAY:
-        AUBase::FillInParameterName(outParameterInfo, PRM_NOTE_DAY_NAME, false);
-        outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-        outParameterInfo.minValue = 0;
-        outParameterInfo.maxValue = 31;
-        outParameterInfo.defaultValue = m_daycount;
-        break;
-      case PRM_NOTE_DAYCOUNT:
-        AUBase::FillInParameterName(outParameterInfo, PRM_NOTE_DAYCOUNT_NAME, false);
-        outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-        outParameterInfo.minValue = 0;
-        outParameterInfo.maxValue = 365;
-        outParameterInfo.defaultValue = m_daycount;
-        break;
-      case PRM_NOTE_MONTH:
-        AUBase::FillInParameterName(outParameterInfo, PRM_NOTE_MONTH_NAME, false);
-        outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-        outParameterInfo.minValue = 0;
-        outParameterInfo.maxValue = 11;
-        outParameterInfo.defaultValue = m_daycount;
-        break;
-      case PRM_NOTE_INDEX:
-        AUBase::FillInParameterName(outParameterInfo, PRM_NOTE_INDEX_NAME, false);
-        outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-        outParameterInfo.minValue = 0;
-        outParameterInfo.maxValue = 100;
-        outParameterInfo.defaultValue = m_index;
-        break;
-         */
-      default:
-        result = AUBase::GetParameterInfo(inScope, inParameterID, outParameterInfo);
-        break;
-    }
-	}
-  else {
-    result = kAudioUnitErr_InvalidParameter;
-  }
-  
-	return result;
-}
-
-ComponentResult AUNotes::GetPropertyInfo(AudioUnitPropertyID inID,
-                                         AudioUnitScope inScope,
-                                         AudioUnitElement inElement,
-                                         UInt32 &outDataSize,
-                                         Boolean &outWritable) {
-	return AUEffectBase::GetPropertyInfo(inID, inScope, inElement, outDataSize, outWritable);
-}
+void AUNotes::GetUIComponentDescs(ComponentDescription* inDescArray) {
+  inDescArray[0].componentType = kAudioUnitCarbonViewComponentType;
+  inDescArray[0].componentSubType = AUNotes_COMP_SUBTYPE;
+  inDescArray[0].componentManufacturer = AUNotes_COMP_MANF;
+  inDescArray[0].componentFlags = 0;
+  inDescArray[0].componentFlagsMask = 0;
+};
 
 ComponentResult AUNotes::SaveState(CFPropertyListRef *outData) {
   ComponentResult err = AUBase::SaveState(outData);
-  
+  // FIXME: Read text here
   return err;
 }
 
 ComponentResult AUNotes::RestoreState(CFPropertyListRef plist) {
   ComponentResult err = AUBase::RestoreState(plist);
-  
+  // FIXME: Set text here
   return err;
-}
-
-ComponentResult AUNotes::GetProperty(AudioUnitPropertyID inID,
-                                     AudioUnitScope inScope,
-                                     AudioUnitElement inElement,
-                                     void *outData) {
-	return AUEffectBase::GetProperty(inID, inScope, inElement, outData);
 }
 
 void AUNotes::AUNotesKernel::Reset() {
 }
 
-void AUNotes::AUNotesKernel::Process(const Float32 *inSourceP,
-                                     Float32 *inDestP,
-                                     UInt32 inFramesToProcess,
-                                     UInt32 inNumChannels,
-                                     bool &ioSilence) {
-#if 1
-  for(unsigned int i = 0; i < inFramesToProcess; ++i) {
-    inDestP[i] = inSourceP[i];
+void AUNotes::AUNotesKernel::Process(const Float32 *inputs, Float32 *outputs,
+                                     UInt32 frames, UInt32 channels, bool &ioSilence) {
+  for(unsigned int i = 0; i < frames; ++i) {
+    outputs[i] = inputs[i];
   }
-#else
-	UInt32 nSampleFrames = inFramesToProcess;
-	const Float32 *sourceP = inSourceP;
-	Float32 *destP = inDestP;
-  
-  // Not sure what the hell all this is necessary for...
-	while(--nSampleFrames > 0) {
-    Float32 inputSample = *sourceP;
-		
-		sourceP += inNumChannels;
-    Float32 outputSample = inputSample;
-    
-		*destP = outputSample;
-		destP += inNumChannels;
-	}
-#endif
 }
--- source/AUNotes.h
+++ source/AUNotes.h
@@ -13,22 +13,9 @@
 #include <AudioUnit/AudioUnitCarbonView.h>
 #include "AUEffectBase.h"
 
-#ifdef MAC
-#include <sys/types.h>
-#include <dirent.h>
-#endif
-
 #ifndef __defaults_H
 #include "defaults.h"
 #endif
-
-/*
-static CFStringRef PRM_NOTE_YEAR_NAME = CFSTR("Year");
-static CFStringRef PRM_NOTE_DAY_NAME = CFSTR("Day");
-static CFStringRef PRM_NOTE_DAYCOUNT_NAME = CFSTR("Daycount");
-static CFStringRef PRM_NOTE_MONTH_NAME = CFSTR("Month");
-static CFStringRef PRM_NOTE_INDEX_NAME = CFSTR("Index");
-*/
  
 class AUNotes : public AUEffectBase {
 public:
@@ -37,33 +24,11 @@ public:
   
   AUKernelBase *NewKernel() { return new AUNotesKernel(this); };
   
-	ComponentResult GetParameterValueStrings(AudioUnitScope inScope,
-                                           AudioUnitParameterID inParameterID,
-                                           CFArrayRef *outStrings);
-	ComponentResult GetParameterInfo(AudioUnitScope inScope,
-                                   AudioUnitParameterID	inParameterID,
-                                   AudioUnitParameterInfo	&outParameterInfo);
-	ComponentResult GetPropertyInfo(AudioUnitPropertyID inID,
-                                  AudioUnitScope inScope,
-                                  AudioUnitElement inElement,
-                                  UInt32 &outDataSize,
-                                  Boolean	&outWritable);
-	ComponentResult GetProperty(AudioUnitPropertyID inID,
-                              AudioUnitScope inScope,
-                              AudioUnitElement inElement,
-                              void *outData);
-  
   bool SupportsTail() { return false; };
 	ComponentResult	Version() { return kAUNotesVersion; };
   
   int GetNumCustomUIComponents() { return 1; };
-	void GetUIComponentDescs(ComponentDescription* inDescArray) {
-    inDescArray[0].componentType = kAudioUnitCarbonViewComponentType;
-    inDescArray[0].componentSubType = AUNotes_COMP_SUBTYPE;
-    inDescArray[0].componentManufacturer = AUNotes_COMP_MANF;
-    inDescArray[0].componentFlags = 0;
-    inDescArray[0].componentFlagsMask = 0;
-	};
+	void GetUIComponentDescs(ComponentDescription* inDescArray);
   
   ComponentResult SaveState(CFPropertyListRef *outData);
   ComponentResult RestoreState(CFPropertyListRef plist);
